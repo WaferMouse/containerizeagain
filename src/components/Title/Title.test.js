c66d84f671c153a22b353392ca1e8e32ae68bde4
4rchie-r <archiereader@outlook.com>
2024-09-05 11:38:26 +0100

    testing the new yml

---
+++ src/components/Title/Title.test.js
@@ -0,0 +1,14 @@
+
+
+
+import { render } from '@testing-library/react';
+import Title from './Title';
+
+test('renders h2 element with correct text', () => {
+    const { getByText } = render(<Title />);
+    const titleElement = getByText('Docs');
+    expect(titleElement.tagName).toBe('h2');
+});
+
+
+
