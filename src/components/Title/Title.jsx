d7992f96bd4d2da5717628516c25e9123e001862
Maria <maria.g1611@yahoo.com>
2024-09-05 12:08:50 +0100

    should look normal

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,12 +1,11 @@
 import { Gideon_Roman } from "next/font/google";
 
 export default function Title() {
-  console.log("wow");
   return (
     <>
-                                 <h2>
+      <h2>
         Docs <span>-&gt;</span>
-                                    </h2>
+      </h2>
     </>
   );
 }
